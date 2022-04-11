1faadf2e2d0727459df074dc06b5ddfdc34d31e6
Stephan Bouman <stephan@creativecycle.nl>
2022-04-11 16:57:17 +0200

    Added toCelcius, toKelvin,  fromKelvin and fromFahrenheit

--- src/Temperature.php
+++ src/Temperature.php
@@ -11,13 +11,34 @@ class Temperature
         return new static($celsius);
     }
 
+    public static function fromKelvin(float $kelvin): self
+    {
+        return new static($kelvin - 273.15);
+    }
+
+    public static function fromFahrenheit(float $fahrenheit): self
+    {
+        return new static((5 / 9) * ($fahrenheit - 32));
+    }
+
+    public function toKelvin(): float
+    {
+        return $this->celsius + 273.15;
+    }
+
     public function __construct(float $celsius)
     {
         $this->celsius = $celsius;
     }
 
+
     public function toFahrenheit(): float
     {
         return ($this->celsius * 1.8) + 32;
+    }
+
+    public function toCelsius(): float
+    {
+        return $this->celsius;
     }
 }
--- tests/TemperatureTest.php
+++ tests/TemperatureTest.php
@@ -14,4 +14,45 @@ class TemperatureTest extends TestCase
 
         $this->assertEquals(212, $fahrenheit);
     }
+
+    /** @test */
+    public function it_converts_fahrenheit_to_celsius()
+    {
+        $celsius = Temperature::fromFahrenheit(212)->toCelsius();
+
+        $this->assertEquals(100, $celsius);
+    }
+
+    /** @test */
+    public function it_converts_kelvin_to_celsius()
+    {
+        $celsius = Temperature::fromKelvin(100)->toCelsius();
+
+        $this->assertEquals(-173.15, $celsius);
+    }
+
+    /** @test */
+    public function it_converts_kelvin_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::fromKelvin(400)->toFahrenheit();
+
+        $this->assertEquals(260.33, $fahrenheit);
+    }
+
+    /** @test */
+    public function it_converts_celsius_to_celcius()
+    {
+        $celsius = Temperature::fromCelsius(200)->toCelsius();
+
+        $this->assertEquals(200, $celsius);
+    }
+
+    /** @test */
+    public function it_converts_celcius_to_kelvin()
+    {
+        $celsius = Temperature::fromCelsius(100)->toKelvin();
+
+        $this->assertEquals(373.15, $celsius);
+    }
+
 }
